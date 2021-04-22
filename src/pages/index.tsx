23961631854658cb132aa31288290b5f043519b4
IcaroRodrigues <Kevin_Lothar@hotmail.com>
2021-04-22 02:19:53 -0300

    feat: adiciona biblioteca axios ao projeto para chamadas api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,30 @@
+import { GetStaticProps } from 'next'
 
+import { api } from '../services/api'
+interface Episode {
+  id: string
+  title: string
+  members: string
+}
 
-export default function Home(props) {
+interface HomeProps {
+  episodes: Episode[]
+}
+
+export default function Home(props: HomeProps) {
   return (
-    <h1>index</h1>
+    <h1>bem vindo</h1>
   )
 }
 
-export async function getStaticProps () {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
 
   return {
     props: {
