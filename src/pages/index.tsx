7966b0be7773a09919b5c6d11e5c89866e16af86
IcaroRodrigues <Kevin_Lothar@hotmail.com>
2021-04-22 23:04:43 -0300

    feat: cria função para converter duração dos episodios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { GetStaticProps } from 'next'
+import { format, parseISO } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 
 import { api } from '../services/api'
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString'
 interface Episode {
   id: string
   title: string
@@ -23,6 +26,20 @@ export const getStaticProps: GetStaticProps = async () => {
       _limit: 12,
       _sort: 'published_at',
       _order: 'desc'
+    }
+  })
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url
     }
   })
 
